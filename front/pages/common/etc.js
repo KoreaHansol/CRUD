664faf729e0ddc027ee943ee39ee8a75b374f88d
KoreaHansol <hskimaa@hanmail.net>
2020-11-06 17:56:19 +0900

    DB나누기 전

--- front/pages/common/etc.js
+++ front/pages/common/etc.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLayout from '../../components/AppLayout';
-
-const guitar = () => {
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Table } from 'antd';
+import Link from 'next/link';
+import { addPost, loadPostAction } from '../../reducers/post';
+const etc = () => {
+  const dispatch = useDispatch();
+  const { mainPosts } = useSelector(state => state.post);
+  useEffect(() => {
+    dispatch(loadPostAction({
+      category: 'etc'
+    }));
+  }, []);
+  const columns = [
+    {
+      title: '번호',
+      width: 100,
+      dataIndex: 'id',
+      key: 'id',
+      render: (text) =>  mainPosts && mainPosts.length,
+    },
+    { 
+      title: '제목', 
+      key: 'title' ,
+      render: (text) => <Link href={`/common/etc/${text.id}`}><a>{text.title}</a></Link>,
+    },
+    {
+      title: '이름',
+      width: 200,
+      dataIndex: 'nickname',
+      key: 'nickname',
+    },
+    {
+      title: '작성일',
+      width: 200,
+      dataIndex: 'updatedAt',
+      key: 'updatedAt',
+    },
+    {
+      title: '조회',
+      width: 100,
+      dataIndex: 'update',
+      key: 'update',
+    },
+    {
+      title: '추천',
+      width: 100,
+      dataIndex: 'update',
+      key: 'update',
+    },
+  ];
+  
   return (
     <AppLayout>
-        Hello guitar
+        <Table columns={columns} dataSource={mainPosts} scroll={{ x: 1300 }} />
+        <Button><Link href="/common/etc/add"><a>글쓰기</a></Link></Button>
     </AppLayout>
   );
 };
 
-export default guitar;
+
+
+export default etc;
--- front/pages/common/etc/[id].js
+++ front/pages/common/etc/[id].js
@@ -1,15 +1,106 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import AppLayout from '../../../components/AppLayout'
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-const Home = () => {
+import { useDispatch, useSelector } from 'react-redux';
+import { addCommentAction, loadsinglePostAction } from '../../../reducers/post';
+import { Descriptions, Comment, Avatar, Form, Button, List, Input, Tooltip } from 'antd';
+
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
+  <>
+    <Form.Item>
+      <Input.TextArea rows={4} onChange={onChange} value={value} />
+    </Form.Item>
+    <Form.Item>
+      <Button htmlType="submit"  onClick={onSubmit} type="primary">
+        댓글등록
+      </Button>
+    </Form.Item>
+  </>
+);
+const etcid = () => {
     const router = useRouter();
+    const dispatch = useDispatch();
+    const [value,setValue] = useState('');
     const { id } = router.query;
+    const { singlePost } = useSelector(state => state.post);
+    const { user } = useSelector(state => state.user);
+    useEffect(() => {
+        if(id)
+            dispatch(loadsinglePostAction(
+              {
+                id,
+                category:'etc'
+              }
+            ));
+    }, [id]);
+    const submitting = () => {
+
+    }
+    const handleSubmit = () => {
+      if(user && user.data) {
+        dispatch(addCommentAction({
+          id: singlePost.data.id,
+          userid: user.data.id,
+          content: value,
+          category: 'etc'
+        }));
+        setValue('');
+      } else {
+        alert('로그인해주세요')
+      }
+      
+    }
+    const handleChange = (e) => {
+      setValue(e.target.value)
+    }
   return (
     <AppLayout>
-     {id}
+        <Descriptions bordered style={{ background:'white' }}>
+          <Descriptions.Item label="글쓴이">
+            {singlePost && singlePost.data ? singlePost.data.nickname : null}
+          </Descriptions.Item>
+          <Descriptions.Item label="수정일" span={2}>
+            {singlePost && singlePost.data ? singlePost.data.createdAt : null}
+          </Descriptions.Item>
+          <Descriptions.Item label="제목" span={3}>
+            {singlePost && singlePost.data ? singlePost.data.title : null}
+          </Descriptions.Item>
+          <Descriptions.Item label="내용" style={{height:'10vh'}} span={3}>
+            {singlePost && singlePost.data ? singlePost.data.content : null}
+          </Descriptions.Item>
+          
+        </Descriptions>
+       
+        <Comment
+          content={
+            <Editor
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+              submitting={submitting}
+              value={value}
+            />
+          }
+        />
+        {singlePost && singlePost.data ? singlePost.data.Comments.map((v, i)=>{
+          return (
+            <Comment
+              author={<a>{v.nickname}</a>}
+              content={
+                <p>
+                  {v.content}
+                </p>
+              }
+              // datetime={
+              //   <Tooltip title='asd'>
+              //     <span>sa</span>
+              //   </Tooltip>
+              // }
+            />
+          )
+        }) : null}
     </AppLayout>
   );
 };
 
-export default Home;
+export default etcid;
